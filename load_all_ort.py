57c2621a50c6cfbffcac284bd449ddde83e3a6fa
Martijn Bentum <bentummartijn@gmail.com>
2017-06-27 17:38:24 +0200

    load_all_ort now contains a function to create fid2ort dictionary: make_fid2ort(), to prevent creating the dict on loading the module the functions loads all ort objects for the experiment and links them to file id as the key

--- load_all_ort.py
+++ load_all_ort.py
@@ -1,37 +1,50 @@
-ifadv = [line.split('\t') for line in open('../fnlist_ifadv.txt').read().split('\n')]
-fno = [line.split('\t') for line in open('../fnlist_o.txt').read().split('\n')]
-fnk = [line.split('\t') for line in open('../fnlist_k.txt').read().split('\n')]
+def make_fid2ort(verbose = True):
+	import ort
+	'''Make fid2ort dictionary that maps file id to ort objects.
 
-o_content_words = 0
-k_ncontent_words = 0
-ifadv_ncontent_words = 0
+	deepcopying this object takes approx 18 seconds while making it takes
+	90 seconds. Options is to make it once and copy it for each participant
+	'''
 
-k = []
-o =[]
-ifadv = []
+	fnifadv = [line.split('\t') for line in open('../fnlist_ifadv.txt').read().split('\n')]
+	fno = [line.split('\t') for line in open('../fnlist_o.txt').read().split('\n')]
+	fnk = [line.split('\t') for line in open('../fnlist_k.txt').read().split('\n')]
 
-for line in fnk:
-    k.append(ort.Ort(fid = line[1],sid_name=line[0],path ='../TABLE_CGN2_ORT/',awd_path = '../../CGN/TABLE_CGN2_AWD/',corpus='CGN',pos_path = 'POS_K/FROG_OUTPUT/',register = 'news_broadcast'))
-    k_ncontent_words += k[-1].speakers[0].ncontent_words
-    print(k[-1])
+	o_ncontent_words = 0
+	k_ncontent_words = 0
+	ifadv_ncontent_words = 0
 
-for line in fno:
-    o.append(ort.Ort(fid = line[1],sid_name=line[0],path ='..//TABLE_CGN2_ORT/',awd_path = '../../CGN/TABLE_CGN2_AWD/',corpus='CGN',pos_path = 'POS_O/FROG_OUTPUT/',register = 'read_aloud_stories'))
-    o_ncontent_words += o[-1].speakers[0].ncontent_words
-    print(o[-1])
+	k = {}
+	o = {}
+	ifadv = {}
+	fid2ort = {}
 
-for line in fnifadv:
-    ifadv.append(ort.Ort(fid = line[2],sid_name=line[0],path ='../IFADV_ANNOTATION/ORT/',awd_path = '../IFADV_ANNOTATION/AWD/WORD_TABLES/',corpus='IFADV',pos_path = 'POS_IFADV/FROG_OUTPUT/',register = 'spontaneous_dialogue'))
-    ifadv[-1].add_speaker(line[1])
-	ifadv[-1].check_overlap()
-    print(ifadv[-1])
-    ifadv_ncontent_words += ifadv[-1].speakers[0].ncontent_words
-    ifadv_ncontent_words += ifadv[-1].speakers[1].ncontent_words
+	for line in fnk:
+		fid2ort[line[1]] = ort.Ort(fid = line[1],sid_name=line[0],path ='../TABLE_CGN2_ORT/',awd_path = '../../CGN/TABLE_CGN2_AWD/',corpus='CGN',pos_path = 'POS_K/FROG_OUTPUT/',register = 'news_broadcast')
+		k_ncontent_words += fid2ort[line[1]].speakers[0].ncontent_words
+		if verbose:
+			print(fid2ort[line[1]])
 
-print('ifadv ncontent',ifadv_ncontent_words)
-print('k ncontent',k_ncontent_words)
-print('o ncontent',o_ncontent_words)
-print('all ncontent',ifadv_ncontent_words+k_ncontent_words+o_content_words)
-print('all times 48 pp',(ifadv_ncontent_words+k_ncontent_words+o_content_words)*48)
+	for line in fno:
+		fid2ort[line[1]] = ort.Ort(fid = line[1],sid_name=line[0],path ='..//TABLE_CGN2_ORT/',awd_path = '../../CGN/TABLE_CGN2_AWD/',corpus='CGN',pos_path = 'POS_O/FROG_OUTPUT/',register = 'read_aloud_stories')
+		o_ncontent_words += fid2ort[line[1]].speakers[0].ncontent_words
+		if verbose:
+			print(fid2ort[line[1]])
 
+	for line in fnifadv:
+		fid2ort[line[2]] = ort.Ort(fid = line[2],sid_name=line[0],path ='../IFADV_ANNOTATION/ORT/',awd_path = '../IFADV_ANNOTATION/AWD/WORD_TABLES/',corpus='IFADV',pos_path = 'POS_IFADV/FROG_OUTPUT/',register = 'spontaneous_dialogue')
+		fid2ort[line[2]].add_speaker(line[1])
+		fid2ort[line[2]].check_overlap()
+		if verbose:
+			print(fid2ort[line[2]])
+		ifadv_ncontent_words += fid2ort[line[2]].speakers[0].ncontent_words
+		ifadv_ncontent_words += fid2ort[line[2]].speakers[1].ncontent_words
+
+	if verbose:
+		print('ifadv ncontent',ifadv_ncontent_words)
+		print('k ncontent',k_ncontent_words)
+		print('o ncontent',o_ncontent_words)
+		print('all ncontent',ifadv_ncontent_words+k_ncontent_words+o_ncontent_words)
+		print('all times 48 pp',(ifadv_ncontent_words+k_ncontent_words+o_ncontent_words)*48)
+	return fid2ort
 
