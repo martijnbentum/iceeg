d411b3218a1ddba9ea88827bdf5dabf9d2cad302
Martijn Bentum <bentummartijn@gmail.com>
2017-07-07 14:11:49 +0200

    path.py defines paths to all data dependencies in the module.

---
+++ util/path.py
@@ -0,0 +1,18 @@
+'''Defines paths to all data dependencies of the module.'''
+
+data= '/Volumes/storage/EEG_DATA_ifadv_cgn/'
+eeg= data + 'EEG/'
+log= data + 'LOG_FILES/'
+marker= data + 'MARKER_FILES/'
+cgn_annot = data + 'CGN_ANNOTATION/' 
+ifadv_annot = data + 'IFADV_ANNOTATION/'
+compo_pos = cgn_annot + 'POS_O/'
+compk_pos = cgn_annot + 'POS_K/'
+ifadv_pos = ifadv_annot + 'POS_IFADV/'
+cgn_ort = cgn_annot + 'TABLE_CGN2_AWD/'
+cgn_awd = cgn_annot + 'TABLE_CGN2_ORT/'
+ifadv_ort = ifdav_annot + 'ORT/'
+ifadv_awd = ifdav_annot + 'AWD/'
+
+
+
