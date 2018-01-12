097c6f93bdc1ba0e2b8bb77e281b2cd17921007b
Bentum <u050158@ip-145-116-184-17.wlan-int.ru.nl>
2018-01-12 14:23:00 +0100

    path.py added new directories and now also sets paths correctly on ponyland

--- iceeg/path.py
+++ iceeg/path.py
@@ -1,13 +1,21 @@
 import os
 '''Defines paths to all data dependencies of the module.'''
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+bak = '/Volumes/BAK4TB/'
 if os.path.isdir('/Volumes/storage/'): volume = 'Volumes/storage/'
 elif os.path.isdir('/Users/u050158/storage/'): volume = '/Users/u050158/storage/'
+elif os.path.isdir('/vol/tensusers/mbentum/'): 
+	volume = '/vol/tensusers/mbentum/'
+	bak = '/vol/tensusers/mbentum/BAK/'
 else: print('please add path to secondary data folder to the path.py file in the iceeg folder.')
 
 data= volume + 'EEG_DATA_ifadv_cgn/'
 if os.path.isdir(volume +'EEG'):eeg= volume + 'EEG/'
 elif os.path.isdir('/Volumes/BAK4TB/EEG/'):eeg = '/Volumes/BAK4TB/EEG/'
 elif os.path.isdir('/Volumes/BAK1TB/EEG/'): eeg = '/Volumes/BAK1TB/EEG/'
+elif os.path.isdir('/vol/tensusers/mbentum/BAK/EEG/'): eeg = '/vol/tensusers/mbentum/BAK/EEG/'
 else: print('please add path to eeg files to path.py file in the iceeg folder')
 
 log= data + 'LOG_FILES/'
@@ -25,16 +33,19 @@ ifadv_awd = ifadv_annot + 'AWD/'
 blinks = data + 'BLINKS/'
 plot_blinks = data + 'PLOT_BLINKS/'
 artifacts = data + 'ARTIFACTS/'
+artifacts_clean = data+ 'ARTIFACTS_CLEAN/'
+snippet_annotation = data + 'SNIPPET_ANNOTATION/'
 data_stats = data + 'DATA_STATS/'
 
 eeg100hz = data + 'EEG100hz/'
 
 video_log = data + 'VIDEO_LOG_EEG/'
-video_data = '/Volumes/BAK4TB/EEG_VIDEO/'
-video_frames= '/Volumes/BAK4TB/EEG_VIDEO_FRAMES/'
-auto_encoder_data = '/Volumes/BAK4TB/AUTO_ENCODER_DATA/'
-artifact_training_data = '/Volumes/BAK4TB/ARTIFACT_TRAINING_DATA/'
-artifact_data_all_pp = '/Volumes/BAK4TB/ARTIFACT_DATA_ALL_PP/'
-models = '/Volumes/BAK4TB/MODELS/'
+video_data = bak + 'EEG_VIDEO/'
+video_frames= bak + 'EEG_VIDEO_FRAMES/'
+auto_encoder_data = bak + 'AUTO_ENCODER_DATA/'
+artifact_training_data = bak + 'ARTIFACT_TRAINING_DATA/'
+artifact_training_dataraw = bak + 'ARTIFACT_TRAINING_DATARAW/'
+artifact_data_all_pp = bak + 'ARTIFACT_DATA_ALL_PP/'
+model = bak + 'MODELS/'
 
-
+artifact_cnn_xml = data + 'ARTIFACT_CNN_XML/'
