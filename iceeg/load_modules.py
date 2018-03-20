baea6030d8a625cbe81a6b4df83c855f80797b2d
Bentum <u050158@c007610.srw.ru.nl>
2018-03-20 13:19:46 +0100

    load_modules 		auto load iceeg modules when in the iceeg folder

---
+++ iceeg/load_modules.py
@@ -0,0 +1,8 @@
+import experiment as e
+import windower
+import utils
+import xml_handler
+import xml_cnn
+import path
+import manual_artifact_coder as mac
+print('loaded: e, mac, windower, utils, xml_handler, xml_cnn, path')
