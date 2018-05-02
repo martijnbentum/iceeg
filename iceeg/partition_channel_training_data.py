79e5fe99a66300d860daaa93a00d0289ce5a80e8
Bentum <u050158@c007610.srw.ru.nl>
2018-05-02 10:41:17 +0200

    partition_channel_training_data 		adapted from partition_training_data 										create part files for training data 										in part files samples from different block 										files are combined into one large file with 										a ratio of clean / artifact of channels similar 										to the overall ratio of clean / artifact in annotated 										EEG block files (approximately 2% bad channels)

---
+++ iceeg/partition_channel_training_data.py
@@ -0,0 +1,148 @@
+import glob 
+import matplotlib.pyplot as plt
+import numpy as np
+import path
+import random
+import scipy.signal as signal
+import time
+
+
+def make_xfold(total_nrows, x = 100):
+	'''Create indices files for part files. Each index file contains the indices for that file to be loaded from h5 data.
+	Since h5 loading was slow (4-6 hours for one clean part file to be made, I created code to convert
+	the aggregated index to block file specific indices, which increased loading significantly ~ 1hour.
+	'''
+	ai = list(range(total_nrows))
+	random.shuffle(ai)
+	start_ai,end_ai = 0,0
+	ai_step = int(len(ai)/x)
+	i_folds = []
+	for i in range(x):
+		end_ai += ai_step
+		print(start_ai,end_ai)
+		if i == x-1: 
+			i_folds.append(ai[start_ai:])
+		else: 
+			i_folds.append(ai[start_ai:end_ai])
+		start_ai += ai_step
+	return i_folds
+
+
+def save_fold_indices(i_folds):
+	'''Save part indices refering to a specific index in a specific block in the block data files.'''
+	for i,f in enumerate(i_folds):
+		print('indices_part-'+str(i+1))
+		np.save(path.channel_artifact_training_data + 'PART_INDICES/indices_part-'+str(i+1),f)
+
+# code below duplicate in make_artifact_matrix_v2.py
+
+def load_dict_datafn2nrows():
+	'''converts a datafilename to the number of rows the corresponding np array has.'''
+	df2n = dict([[line.split('\t')[0],int(line.split('\t')[1])] for line in open(path.data + 'channel_nrows_per_block','r').read().split('\n') if line])
+	return df2n
+
+
+def make_list_datafn_all_rows_before(datafn2nrows):
+	'''Computes the number of data rows come before the current file.
+	= nrows of each file occuring in the glob.glob list of datafiles.''
+	'''
+	datafn_all_rows_before =[] 
+	all_rows_before = 0
+	for k in datafn2nrows:
+		datafn_all_rows_before.append([k,  all_rows_before])
+		all_rows_before += datafn2nrows[k]
+	return datafn_all_rows_before
+
+def find_correct_np_file(datafn_all_rows_before,all_index):
+	'''return filename of np array that contains the all_index, and number of data rows before this file.
+	all_index is the index of the epoch in the h5 file of all data file combined.
+	the all index is used to find the block data file that contains this epoch.
+	'''
+	for i,line in enumerate(datafn_all_rows_before):
+		if i < len(datafn_all_rows_before) -1:
+			if datafn_all_rows_before[i+1][1] > all_index >= line[1]:
+				# print('found file',all_index,line[1],datafn_all_rows_before[i+1],i)
+				return line
+		elif  17124221 > all_index >= line[1]:
+			# print('found file',all_index,line[1],i)
+			return line
+		else:
+			raise ValueError('File not found',all_index,i)
+			
+
+def convert_index_all_2_specific(nrows_before,all_index):
+	'''Converts the all_index to the index in the original block data file.'''
+	return all_index - nrows_before
+
+def make_part_file(filenames_and_indices):
+	'''creates a part data file based on the filenames_and_indices dictionary.
+	the filenames_and_indices links filenames to the indices of the epochs that are needed for the current part file.
+	The function loads each filename and extract the corresponding indices and puts them in the output np.array which is returned.
+	'''
+	nrows = sum([len(filenames_and_indices[f]) for f in filenames_and_indices])
+	data_output = np.zeros((nrows,2600))
+	info_output = np.zeros((nrows,26))
+	start_index = 0
+	for i,f in enumerate(filenames_and_indices):
+		print(f,i,len(filenames_and_indices))
+		d = np.load(path.channel_artifact_training_data + f.replace('info','data'))
+		info = np.load(path.channel_artifact_training_data + f)
+		temp = d[filenames_and_indices[f],:]
+		itemp = info[filenames_and_indices[f],:]
+		data_output[start_index:start_index+temp.shape[0],:] = temp
+		info_output[start_index:start_index+temp.shape[0],:] = itemp
+		start_index += temp.shape[0]
+	return data_output,info_output
+
+def make_filenames_and_indices(indices,datafn_all_rows_before):
+	'''Create a dictionary that links a filename to the indices of the epochs in that np array that are needed for a part file.'''
+	filenames_and_indices = {}
+	for i in indices:
+		f,nrows_before = find_correct_np_file(datafn_all_rows_before,i)
+		ci = convert_index_all_2_specific(nrows_before,i)
+		if f in filenames_and_indices:
+			filenames_and_indices[f].append(ci)
+		else: filenames_and_indices[f] = [ci]
+		# print(i,len(indices_clean1))
+	return filenames_and_indices
+
+def write_update(f):
+	'''Write the current part file in a text file to follow the progress.'''
+	fout = open(path.data + '/part_progress.txt','a')
+	fout.write(f+','+ time.asctime( time.localtime(time.time()) ) +'\n')
+	fout.close()
+
+def make_all_parts():
+	'''Makes 100 part data files. Artifact parts were loaded from h5 file (clean part are 14X bigger) and were
+	prohibitevily slow. Load data from original block data files speeds it up to 1 hour per file instead of 4-6.'''
+	datafn2nrows = load_dict_datafn2nrows()
+	datafn_all_rows_before = make_list_datafn_all_rows_before(datafn2nrows)
+	fn = glob.glob(path.channel_artifact_training_data + 'PART_INDICES/indices*.npy')
+	for i,f in enumerate(fn):
+		print(f,i,len(fn))
+		write_update(f)
+		indices = np.load(f)
+		filenames_and_indices = make_filenames_and_indices(indices,datafn_all_rows_before)
+		data_output,info_output = make_part_file(filenames_and_indices)
+		np.save(f.replace('indices','data'),data_output)
+		np.save(f.replace('indices','info'),info_output)
+		write_update(f.replace('indices_',''))
+
+
+def make_smalltest(nrow_selection = 10000):
+	number_part = map(str,list(range(1,11)))
+	for n,i in enumerate(range(1,100,10)):
+		print('loading: data and info part-',str(i) , '.npy')
+		d = np.load('data_part-'+str(i) + '.npy')
+		i = np.load('info_part-'+str(i) + '.npy')
+		nrows = d.shape[0]
+		indices = random.sample(range(nrows),nrow_selection)
+		sd = d[indices,:]
+		si = i[indices,:]
+		print('saving: data and info part-',number_part[n] , '.npy')
+		np.save('smalltest_data_part-'+number_part[n]+'.npy',sd)
+		np.save('dmalltest_info_part-'+number_part[n]+'.npy',si)
+		
+
+
+
