model7 		calculation of variable dimensions was not based on parameters 			passed to function, fixed this.

iceeg/model7.py
@@ -1,3 +1,4 @@
+import math
 import tensorflow as tf
 
 
@@ -34,7 +35,7 @@ def next_batch(data,info, batch_size):
 	return data[indices,:],info[indices,:]
 
 
-def deepnn(x,nchannels,nsamples,fmap_size = 2400):
+def deepnn(x,nchannels,nsamples,kernel_size,fmap_size = 2400):
 	"""deepnn builds the graph for a deep net for classifying digits.
 	Args:
 		x: an input tensor with the dimensions (N_examples, 784), where 784 is the
@@ -67,7 +68,7 @@ def deepnn(x,nchannels,nsamples,fmap_size = 2400):
 
 	# Second convolutional layer -- maps 32 feature maps to 64.
 	with tf.name_scope('conv2'):
-		W_conv2 = weight_variable([6, 1, 32, 64])
+		W_conv2 = weight_variable([kernel_size, 1, 32, 64])
 		b_conv2 = bias_variable([64])
 		h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2) + b_conv2)
 		print(W_conv2,'W_conv2')
@@ -95,12 +96,13 @@ def deepnn(x,nchannels,nsamples,fmap_size = 2400):
 	# Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
 	# is down to 7x7x64 feature maps -- maps this to 1024 features.
 	with tf.name_scope('fc1'):
-		W_fc1 = weight_variable([8 * 25 * 128, fmap_size])
+		height = math.ceil((math.ceil(nchannels)/2)/2)
+		W_fc1 = weight_variable([height * 25 * 128, fmap_size])
 		b_fc1 = bias_variable([fmap_size])
 		print(W_fc1,'W_fc1')
 		print(b_fc1,'b_fc1')
 
-		h_pool2_flat = tf.reshape(h_pool2, [-1,8 * 25* 128],name = 'h_pool3_flat-bla')
+		h_pool2_flat = tf.reshape(h_pool2, [-1, height* 25* 128],name = 'h_pool3_flat-bla')
 		h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1,name='h_fc1-bla')
 		print(h_pool2_flat,'h_pool2_flat')
 		print(h_fc1,'h_fc1')