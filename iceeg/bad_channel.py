cd68c9be300d9e1d6bfce44f79cf99eece45d67e
Martijn Bentum <bentummartijn@gmail.com>
2018-09-06 10:38:20 +0200

    bad_channel 		added functionality to easily correct bad channel annotations 					search for zero crossing of the channel

--- iceeg/bad_channel.py
+++ iceeg/bad_channel.py
@@ -108,11 +108,11 @@ class Bad_channel(bad_epoch.Bad_epoch):
 		'''Check whether bad epoch and its boundaries are in the plot window defined by start and end.'''
 		self.visible = False
 		if self.ok:
-			if self.end.x > start and self.end.x < end:
+			if self.end.x >= start and self.end.x <= end:
 				self.visible = True
 			if self.start.x < end and self.start.x > start:
 				self.visible = True
-			if self.start.x < start and self.end.x > end:
+			if self.start.x <= start and self.end.x >= end:
 				self.visible = True
 			self.start.in_plot_epoch(start,end)
 			self.end.in_plot_epoch(start,end)
@@ -144,7 +144,76 @@ class Bad_channel(bad_epoch.Bad_epoch):
 		self.set_info()
 		if self.ok and type(channel_data) == np.ndarray: self.plot(channel_data)
 
-		
+
+	def expand_to_zero(self, channel_data, seek_distance = 100):
+		self.zero_crossing(channel_data,'start', seek_distance)
+		self.zero_crossing(channel_data,'end', seek_distance)
+
+	def zero_crossing(self,channel_data,boundary_type='start',seek_distance=100):
+		d = channel_data
+		# set boundary(start or end)
+		if boundary_type == 'start': 
+			boundary = self.start
+			if boundary.x < seek_distance: start_index = 0
+			else: start_index = boundary.x - seek_distance
+			end_index = boundary.x
+			seek = 'before'
+		elif boundary_type == 'end':
+			boundary = self.end
+			cds = channel_data.shape[0]
+			if  cds - boundary.x < seek_distance: end_index = cds-1
+			else: end_index= boundary.x + seek_distance
+			start_index = boundary.x
+			seek = 'after'
+		else: raise ValueError('unknown boundary type',boundary_type)
+		if channel_data[boundary.x] < 1 and channel_data[boundary.x] > -1: 
+			# boundary is already close to zero, do nothing
+			print(boundary,channel_data[boundary.x],boundary.x,'do nothing')
+			return
+		thres = 3
+		while 1:
+			# find indices where channel is close to 0
+			zero_indices = np.where((d < thres) & (d > -1*thres))[0]
+			cz, cd = find_closest_zero(zero_indices,boundary.x,seek)
+			closest_zero, closest_diff = cz, cd
+			if closest_diff < seek_distance:
+				new_x = closest_zero
+				print('found closest zeros:',closest_zero,closest_diff)
+				break
+			else: thres *= 1.5
+		self.set_boundary(boundary,new_x)
+
+
+	def set_boundary(self,boundary, new_x):
+		boundary.old_boundary = boundary.x
+		boundary.x = new_x
+		self.set_info()
+		self.set_complete_replot()
+
+	def expand_to_bad_channel(self,other):
+		if other == None and type(other) != bad_epoch.Boundary: 
+			print('cannot expand to:',other==None,type(other)!=bad_epoch.Boundary)
+			return
+		if other.end.x < self.start.x: self._expand_to_before(other)
+		elif self.start.x < other.end.x: self._expand_to_next(other)
+
+	def _expand_to_before(self,other):
+		if other.end.x >= self.start.x: return 
+		else: self.set_boundary(self.start, other.end.x)
+
+	def _expand_to_next(self,other):
+		if self.end.x >= other.start.x: return 
+		else: self.set_boundary(self.end, other.start.x)
+			
+	def reset_old_boundaries(self):
+		start, end = self.start, self.end
+		if hasattr(start,'old_boundary'):
+			 start.x,start.old_boundary = start.old_boundary,start.x
+		if hasattr(end,'old_boundary'):
+			 end.x,end.old_boundary = end.old_boundary,end.x
+		self.set_info()
+		self.set_complete_replot()
+
 
 	def del_boundary(self,boundary):
 		'''Delete the boundary that equals boundary or delete the boundary specified by the string 'start' or 'end'.'''
@@ -164,3 +233,21 @@ class Bad_channel(bad_epoch.Bad_epoch):
 		if self.ok:
 			return self.start.x * multiplier, self.end.x * multiplier, self.end.x * multiplier - self.start.x * multiplier
 		else: return None
+
+
+
+def find_closest_zero(zero_indices,x,seek):
+	closest_zero = 'Na'
+	zero_distance = 10**8
+	closest_diff = 10**8
+	for zeroi in zero_indices:
+		diff = x - zeroi
+		if seek == 'before' and diff > 0 and diff < zero_distance: 
+			closest_zero = zeroi
+			zero_distance= diff
+		elif seek == 'after' and diff < 0 and abs(diff) < zero_distance: 
+			closest_zero = zeroi
+			zero_distance= abs(diff)
+	return closest_zero, zero_distance
+
+
