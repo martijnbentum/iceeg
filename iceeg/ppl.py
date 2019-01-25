ca6d9766512ccda16cf9b7919b8916b590580d89
mb <mb@mbs-iMac.home>
2019-01-25 09:50:42 +0100

    ppl 		add logprob information to word object (word.py), added register specific logprob (besides cow logprob)

--- iceeg/ppl.py
+++ iceeg/ppl.py
@@ -12,6 +12,7 @@ class word2ppl:
 		self.words = []
 		self.set_ppl_sentences()
 		self._extract_info()
+		self.set_word_index()
 
 
 	def __repr__(self):
@@ -34,11 +35,14 @@ class word2ppl:
 		self.ppls, holds the output of the SRILM ngram call
 		'''
 		exp = self.exp
+		if exp == 'o': self.lm = 'books_30'
+		if exp == 'k': self.lm = 'autocues_30'
+		if exp == 'ifadv': self.lm = 'compa_20'
 		self.infos = [line.split('\t') for line in open(path.data +'sentence_info_' + exp,encoding = 'utf-8').read().split('\n')]
 		self.strings = open(path.data+'sentence_strings_' + exp,encoding = 'utf8').read().split('\n')
 		# should make ppl filename more genral
-		self.ppls = open(path.data+'ppl_' + exp,encoding = 'utf-8').read().split('\n\n')
-
+		self.ppls = open(path.data+'out_' + exp + '_10.ppl',encoding = 'utf-8').read().split('\n\n')
+		self.ppls_register = open(path.data + 'cow_' + self.lm + '.out',encoding = 'utf-8').read().split('\n\n')
 
 	def set_ppl_sentences(self):
 		'''Create ppl_sentence objects.'''
@@ -48,8 +52,9 @@ class word2ppl:
 				setattr(self,self.exp + '_ppl', ppl_line)
 				break
 			info_line = self.infos[i]
+			info_line_register = self.ppls_register[i]
 			s = self.strings[i]
-			self.sentences.append(ppl_sentence(ppl_line,info_line,s,self.exp))
+			self.sentences.append(ppl_sentence(ppl_line,info_line,s,self.exp,info_line_register))
 			self.words.extend(self.sentences[-1].words)
 
 	def _extract_info(self):
@@ -62,11 +67,97 @@ class word2ppl:
 		self.ppl_score= float(self.ppls[-1].split('ppl= ')[1].split(' ')[0])
 		self.ppl1_score= float(self.ppls[-1].split('ppl1= ')[-1].strip())
 		
+	def _make_blocks(self):
+		self.blocks = []
+		for i,s in enumerate(self.sentences):
+			if i == 0: 
+				bid = s.bid
+				temp = [s]
+			elif bid != s.bid:
+				self.blocks.append(temp)
+				temp = [s]
+				bid = s.bid
+			else: temp.append(s)
+			if i == len(self.sentences) -1: self.blocks.append(temp) 
+
+	def set_word_index(self):
+		self._make_blocks()
+		self.blocks_words = []
+		for b in self.blocks:
+			i = 0
+			temp = []
+			for s in b:
+				for w in s.words:
+					w.word_index = i
+					i += 1
+					temp.append(w)
+			self.blocks_words.append(temp)
+
+	def _match_block_word(self,b):
+		if not hasattr(self,'blocks_words'): self.set_word_index()
+		self.matched_block_word = False
+		for bi,bppl in enumerate(self.blocks_words):
+			mismatch = False
+			for i,wppl in enumerate(bppl):
+				if b.words[i].word_utf8_nocode_nodia().lower() != wppl.word: 
+					mismatch = True
+					# print(b.words[i].word_utf8_nocode_nodia(),wppl.word)
+					break
+			if not mismatch: 
+				self.matched_block_word = bppl
+				self.mbwi= bi
+				break
+		if not self.matched_block_word:
+			print('could not match block by word')
+			self.mbwi = False
+
+	def _match_block_sentence(self,b):
+		self.matched_block_sentence = False
+		for bi,bs in enumerate(self.blocks):
+			mismatch = False
+			for i, s in enumerate(bs):
+				if b.sentences[i].string_utf8_words_no_diacritics(True).lower() != s.sentence:
+					mismatch = True
+					# print(i,b.sentences[i],s.sentence)
+					break
+			if not mismatch:
+				self.matched_block_sentence = bs
+				self.mbsi = bi
+				break
+		if not self.matched_block_sentence:
+			print('could not match block by sentence',b)
+			self.mbws = False
+
+	def add_ppl_to_words(self,b):
+		self._match_block_word(b)
+		self._match_block_sentence(b)
+		if not self.matched_block_word and not self.matched_block_sentence:
+			print('cannot add ppl to word without matched block.')
+			return
+		if self.matched_block_word and self.matched_block_sentence and self.mbwi != self.mbsi:
+			print('found a different block  with sentence match and word match')
+			return
+		if self.matched_block_word:
+			for i,wppl in enumerate(self.matched_block_word):
+				if wppl.word != b.words[i].word_utf8_nocode_nodia().lower():
+					raise ValueError('words do not match:',wppl.word,b.words[i],b)
+				b.words[i].ppl = wppl
+		elif self.matched_block_sentence:
+			print('only sentence match between blocks and ppl')
+			for i, sppl in enumerate(self.matched_block_sentence):
+				if sppl.sentence != b.sentences[i].string_utf8_words_no_diacritics(True).lower():
+					raise ValueError('sentences do not match:',sppl,b.sentences[i])
+				for wi,word in enumerate(sppl.words):
+					b.sentences[i].words[wi].ppl = word
+					
+		
+				
+		
 
 
 class ppl_sentence:
 	'''Holds information about each sentence, based on the SRILM ngram call output.'''
-	def __init__(self,ppl_line,info_line,s,exp):
+	def __init__(self,ppl_line,info_line,s,exp,info_line_register = ''):
 		'''PPL information for each sentence, hold ppl_word objects.
 		ppl_line 	a string with all info related to 1 sentence (SRILM ngram output)
 		info_line 	list with sentence info (checks ppl info)
@@ -79,8 +170,11 @@ class ppl_sentence:
 		assert self.info[-1] == self.ppl_list[0] == s
 		self.sentence = s
 		self.exp = exp
+		self.info_line_register = info_line_register
+		self.ppl_register_list = info_line_register.split('\n')
 		self.sentence_number = self.info[1]
 		self.sid = self.info[2]
+		self.bid = self.info[3]
 		self._extract_sentence_ppl()
 		self._extract_noovs()
 		self.set_ppl_words()
@@ -92,27 +186,34 @@ class ppl_sentence:
 		return m
 
 	def __str__(self):
-		m += 'sentence:\t'+self.sentence+ '\n'
+		m = 'sentence:\t'+self.sentence+ '\n'
 		m += 's number:\t'+self.sentence_number+ '\n'
 		m += 'id:\t\t' + self.sid+ '\n'
 		m += 'logprob:\t\t' + str(self.logprob)+ '\n'
 		m += 'ppl:\t\t' + str(self.ppl_score)+ '\n'
 		m += 'ppl1:\t\t' + str(self.ppl1_score)+ '\n'
 		m += 'experiment:\t'+self.exp+ '\n'
+		return m
 
 	def set_ppl_words(self):
 		'''Create ppl_word object for each word in the sentence.'''
 		self.words = []
+		ppl_register_list = self.ppl_register_list[1:-2]
 		for i,word_line in enumerate(self.ppl_list[1:-2]):
-			print(word_line)
+			# print(word_line)
 			word = word_line.split('( ')[-1].split(' | ')[0]
+			word_line_register = ppl_register_list[i]
 			if word == '</s>': continue
-			self.words.append(ppl_word(i,word_line,self))
+			self.words.append(ppl_word(i,word_line,self,word_line_register))
 
 	def _extract_sentence_ppl(self):
 		'''Retreive some sentence ppl stats.'''
 		self.logprob = self.ppl_line.split(' logprob= ')[-1].split(' ')[0]
 		self.ppl_score, self.ppl1_score = self.ppl_line.split('ppl= ')[-1].split(' ppl1= ')
+
+		self.logprob_register = self.info_line_register.split(' logprob= ')[-1].split(' ')[0]
+		self.ppl_score_register, self.ppl1_score_register = self.info_line_register.split('ppl= ')[-1].split(' ppl1= ')
+
 		try: self.ppl_score = float(self.ppl_score)
 		except: pass
 		try: self.ppl1_score = float(self.ppl1_score)
@@ -120,29 +221,47 @@ class ppl_sentence:
 		try: self.logprob = float(self.logprob)
 		except: pass
 
+		try: self.ppl_score_register = float(self.ppl_score_register)
+		except: pass
+		try: self.ppl1_score_register = float(self.ppl1_score_register)
+		except: pass
+		try: self.logprob_register = float(self.logprob_register)
+		except: pass
+
 	def _extract_noovs(self):
 		'''Count out of vocabulary words.'''
 		self.oov = self.ppl_line.split(' words, ')[-1].split(' ')[0]
 		self.nwords= self.ppl_line.split(' sentences, ')[-1].split(' ')[0]
+
+		self.oov_register = self.info_line_register.split(' words, ')[-1].split(' ')[0]
+		self.nwords_register = self.info_line_register.split(' sentences, ')[-1].split(' ')[0]
+
 		try: self.oov = int(self.oov)
 		except: pass
 		try: self.nwords= int(self.nwords)
 		except: pass
 
+		try: self.oov_register = int(self.oov_register)
+		except: pass
+		try: self.nwords_register= int(self.nwords_register)
+		except: pass
+
 
 class ppl_word:
 	'''PPL info for a word based on SRILM ngram call output.'''
-	def __init__(self,i,word_line,sentence):
+	def __init__(self,i,word_line,sentence,word_line_register):
 		'''PPL info for a word based on SRILM ngram call output.
 		i 		index, word number in sentence
 		word... the line corresponding to the word in the SRILM output
 		sent... the string representation of the sentence
 		'''
 		self.ppl_word = word_line.split('( ')[-1].split(' | ')[0]
+		self.ppl_word_register = word_line_register.split('( ')[-1].split(' | ')[0]
 		if self.ppl_word == '<unk>': self.unk = True
 		else: self.unk = False
 		self.index = i
 		self.word_line = word_line
+		self.word_line_register = word_line_register
 		self.ppl_sentence = sentence
 		self.sentence = self.ppl_sentence.sentence
 		self.word = self.sentence.split(' ')[i]
@@ -166,6 +285,7 @@ class ppl_word:
 		m += 'experiment:\t'+self.exp+ '\n'
 		m += 'sentence:\t'+self.sentence+ '\n'
 		m += 'history:\t'+' '.join(self.history) + '\n'
+		return m
 
 	def _extract_metrics(self):
 		'''Retreive ppl information.'''
@@ -178,7 +298,12 @@ class ppl_word:
 		self.p = self.word_line.split('] ')[-1].split(' ')[0]
 		self.logprob = self.word_line.split(' [ ')[-1].split(' ]')[0]
 		self.ngram = int(self.ngram)
+
+		self.p_register = self.word_line_register.split('] ')[-1].split(' ')[0]
+		self.logprob_register = self.word_line_register.split(' [ ')[-1].split(' ]')[0]
+
 		self.p= float(self.p)
 		self.logprob= float(self.logprob)
 		
-
+		self.p_register= float(self.p_register)
+		self.logprob_register= float(self.logprob_register)
